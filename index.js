e767eca869f6f5986b34b578719531db0389ab71
Hernan_Hernandezz <andreshernandez0827@gmail.com>
2020-11-25 23:39:39 -0500

    imagenes con direciones web para facilitar el desarrollo

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var texto = document.getElementById("texto_linea");
 var boton = document.getElementById("botoncito");
 a =boton.onclick
-
-
 function dibujoporclick(){
   var linea = prompt(texto.nodeValue);
   alert(linea)
