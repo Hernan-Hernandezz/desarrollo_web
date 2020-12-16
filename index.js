8c9adc7ab5709596ade484a6146bde4175d3a306
Hernan_Hernandezz <andreshernandez0827@gmail.com>
2020-12-16 10:36:21 -0500

    agregue nuevas carpetas (imagenes,fonts) renombre archivos index.js a main.js cambie index.css a style.css

--- index.js
+++
@@ -1,9 +0,0 @@
-var texto = document.getElementById("texto_linea");
-var boton = document.getElementById("botoncito");
-a =boton.onclick
-function dibujoporclick(){
-  var linea = prompt(texto.nodeValue);
-  alert(linea)
-
-}
-
